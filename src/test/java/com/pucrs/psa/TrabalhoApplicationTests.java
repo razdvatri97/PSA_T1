574a4ee54baca71759b25706170a22cbb147fad4
Josias Melo Vidal <Josias.Vidal@edu.pucrs.br>
2023-03-29 21:34:20 -0300

    Commit inicial com o projeto criado do Spring Init

---
+++ src/test/java/com/pucrs/psa/TrabalhoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pucrs.psa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabalhoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
